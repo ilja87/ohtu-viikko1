57177b2e78f66ea3a2b5138b95911a0fb02b718c
Ilja <Ilja.Ikonen@helsinki.fi>
2018-11-01 08:28:07 +0200

    Kommentti lisatty netbeansissa

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -1,7 +1,7 @@
 package ohtu.ohtuvarasto;
 
 public class Varasto {
-
+  //Kyseinen kommentti on lisatty NetBeansissa
     // --- piilotettu tietorakenteen toteutus: ---
     private double tilavuus;  // paljonko varastoon mahtuu,  > 0
     private double saldo;     // paljonko varastossa on nyt, >= 0
